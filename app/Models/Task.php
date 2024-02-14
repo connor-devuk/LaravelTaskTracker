a86df2289b6d88de52d2639e119e834536d5a4e4
politeuk <connorgoodallbusiness@gmail.com>
2024-02-14 20:18:15 +0000

    [Tasks] Model & Migration along with User relation

---
+++ app/Models/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'category',
+        'due_by',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'due_by' => 'datetime',
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2024_02_14_200928_create_tasks_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->enum('status', ['pending', 'complete', 'overdue'])->default('pending');
+            $table->string('category');
+            $table->timestamp('due_by');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
