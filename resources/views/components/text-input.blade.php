04080ebb692ae2c270eb686d69da92f6ff180071
politeuk <connorgoodallbusiness@gmail.com>
2024-02-15 19:01:17 +0000

    [Components] Modal + Secondary Button + Text Inputs (re-styled)

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-[#1a1a1a] dark:text-gray-300 focus:border-[#e4ff4a] dark:focus:border-[#e4ff4a] focus:ring-[#cce443] dark:focus:ring-[#cce443] rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-[#1a1a1a] dark:text-gray-300 focus:border-[#e4ff4a] dark:focus:border-[#e4ff4a] focus:ring-[#cce443] dark:focus:ring-[#cce443] rounded-md shadow-sm w-full']) !!}>
